510f031ec23558661622491d00590d775bf3aabd
akonyukhov <akonyukhov@phoenixit.ru>
2022-06-18 18:34:11 +0300

    get method for cars

--- src/main/java/com/school21/euphoria/controllers/CarController.java
+++ src/main/java/com/school21/euphoria/controllers/CarController.java
@@ -5,9 +5,7 @@ import com.school21.euphoria.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,8 +27,8 @@ public class CarController {
         model.addAttribute("allCars", allCars);
 
         //Путь до странички на фронте
-        String pathView = "";
-        return pathView;
+//        String pathView = "";
+        return "helloWorld";
     }
 
     @PostMapping
